fd07aec09276fd70ca4d092fbcb0618b85836851
Sonu Kumar <kumarsonu448866@gmail.com>
2026-06-13 01:02:01 +0530

    init database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
 const express = require("express");
 require("./config/database");
-const { connectDB } = require("./config/database");
-
+const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  try {
+    //dummy data
+    const userObj = {
+      firstName: "Sonu",
+      lastName: "Kumar",
+      emailId: "kumarsonu448866@gmail.com",
+      password: "Sonu123",
+      age: 23,
+      gender: "Male",
+    };
+    //creating a new instance of the User Model
+    const user = new User(userObj);
+    await user.save();
+    res.send("User added Successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
+});
 
 //ensures that database connection is established before starting the server
 connectDB()
