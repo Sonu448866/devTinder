f53b279b87c797cdcd1dd687b5b2180dd519e7cd
Sonu Kumar <kumarsonu448866@gmail.com>
2026-06-13 12:58:33 +0530

    created some more api's

--- src/app.js
+++ src/app.js
@@ -49,13 +49,65 @@ app.get("/user", async (req, res) => {
 app.get("/feed", async (req, res) => {
   try {
     const users = await User.find({}); //no filter added
-    if (users.lenght == 0) {
+    if (users.length == 0) {
       res.send("No user Found");
     } else {
       res.send(users);
     }
   } catch (err) {
     res.status(400).send("Some Error is Occured");
+  }
+});
+
+//finding user by ID
+app.get("/user/:ID", async (req, res) => {
+  const userId = req.params.ID;
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      res.send("User not Found");
+    } else {
+      res.status(200).send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went Wrong" + err.message);
+  }
+});
+
+//Finding user by Id and delete it
+app.delete("/user", async (req, res) => {
+  {
+    const userId = req.body.userId;
+    try {
+      // const user = await User.findByIdAndDelete({ _id: userId });
+      //Above and below lines infer the same
+      const user = await User.findByIdAndDelete(userId);
+      if (!user) {
+        res.status(200).send("User not Found");
+      } else {
+        res.status(200).send("User deleted Successfully");
+      }
+    } catch (err) {
+      res.status(400).send("Something went Wrong");
+    }
+  }
+});
+
+//Update the document
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "before",
+    });
+    if (!user) {
+      res.status(200).send("User not Found");
+    } else {
+      res.send("User Updated successfully");
+    }
+  } catch (err) {
+    res.status(400).send("Something went Wrong");
   }
 });
 
